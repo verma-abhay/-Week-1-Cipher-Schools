fdae846fcab9ea69da1c328663d0260261307181
Abhay Verma <125754516+verma-abhay@users.noreply.github.com>
2023-06-24 23:08:45 +0530

    Add files via upload

---
+++ Lecture-5/IncreDecre.java
@@ -0,0 +1,18 @@
+public class IncreDecre {
+    public static void main(String[] args) {
+       //Post increment
+        int m=5;
+        int n=m++;
+        System.out.println("m is "+m+" "+"n is "+n);
+     //pre increment
+         int p=5;
+        int q=++p;
+        System.out.println("p is "+p+" "+"q is "+q);
+
+        //Decrement
+        int a=5;
+        int b=--a;               //this is pre decrement
+        System.out.println("a "+a+" b "+b);
+    }
+    
+}
